124373c2df0fb85ef8a9d00ee7f54d4ecb369916
BoMarconiHenriksen <bomarconi@gmail.com>
2019-01-25 15:46:06 +0100

    Routing modul nr 14.

--- routing/code_examples/src/components/user/User.vue
+++ routing/code_examples/src/components/user/User.vue
@@ -1,4 +1,20 @@
 <template>
+<div>
     <h1>The User Page</h1>
-
+    <hr>
+    <button @click="navigateToHome" class="btn btn-primary">Go To Home</button>
+    <hr>
+    <router-view></router-view> <!-- Gør det muligt at loade subroutes. -->
+    
+</div>
 </template>
+
+<script>
+    export default {
+        methods: {
+            navigateToHome() {
+                this.$router.push('/'); /* Pusher det på stacks af de eksisterende routes. Gør at frem og tilbage virker. */
+            }
+        }
+    }
+</script>
